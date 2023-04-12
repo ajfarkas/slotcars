3454585f0d0dd38b7cedc70bff701180dc4fcbf2
AJ Farkas <farkas.aj@gmail.com>
2023-04-11 21:35:37 -0400

    forward-back + generic turn
    Turn is not from point of camera.

--- kart.js
+++ kart.js
@@ -1,4 +1,5 @@
-const SPEED = 10;
+const SPEED = 5;
+const TURN_SENSITIVITY = .2;
 
 const $id = (identify) => document.getElementById(identify);
 const getStyle = (value, el) =>
@@ -11,18 +12,48 @@ const board = $id("board");
 const PERSPECTIVE = parseInt(getStyle('perspective', world));
 const BOARD_H = parseInt(getStyle("height", world));
 const SOUTH_POINT = -2 * PERSPECTIVE;
-const BOARD_TRANS = `rotateX(90deg) translateZ(${PERSPECTIVE * 2}px)`;
+const BOARD_TRANS = `rotateX(105deg) translateZ(${PERSPECTIVE * 2}px)`;
 
 board.style.transform = `${BOARD_TRANS}  translateY(${SOUTH_POINT}px)`;
 
 let NORTH = SOUTH_POINT + 0;
-const maxMotion = SOUTH_POINT + BOARD_H;
-const moveForward = () => {
-  NORTH += SPEED;
-  if (NORTH <= maxMotion) {
-    board.style.transform = `${BOARD_TRANS} translateY(${NORTH}px)`;
-  } else {
-    clearInterval(forward);
-  }
-};
-const forward = setInterval(moveForward, 15);
+let ANGLE = 0;
+
+const activeKeys = [];
+document.addEventListener('keyup', ev => {
+  const { key } = ev;
+  if (!key.match('Arrow')) return;
+  delete activeKeys[key];
+});
+document.addEventListener('keydown', ev => {
+  const { key } = ev;
+  if (!key.match('Arrow')) return;
+  activeKeys[key] = true;
+});
+const move = setInterval(() => {
+  Object.entries(activeKeys).forEach(([k, v]) => {
+    if (!v) return;
+    if (k === 'ArrowUp') {
+      NORTH += SPEED;
+    }
+    if (k === 'ArrowLeft') {
+      if (activeKeys.ArrowDown) {
+        ANGLE -= TURN_SENSITIVITY;
+      } else {
+        ANGLE += TURN_SENSITIVITY;
+      }
+    };
+    if (k === 'ArrowDown') {
+      NORTH -= SPEED;
+    };
+    if (k === 'ArrowRight') {
+      if (activeKeys.ArrowDown) {
+        ANGLE += TURN_SENSITIVITY;
+      } else {
+        ANGLE -= TURN_SENSITIVITY;
+      }
+    };
+  });
+
+  board.style.transform = `${BOARD_TRANS} translateY(${NORTH}px) rotateZ(${ANGLE}deg)`;
+}, 15);
